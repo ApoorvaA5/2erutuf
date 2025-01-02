6d735ff264a93ab61cec9be1c41559a29b7c6e66
apoorvaa <apoorvaagowda08@gmail.com>
2025-01-02 15:06:41 +0530

    Investment Data

--- src/components/InvestmentPhilosophy/data.ts
+++ src/components/InvestmentPhilosophy/data.ts
@@ -2,37 +2,37 @@ export const companyLogos = [
   {
     id: 1,
     name: 'Connect Pro',
-    logo: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?auto=format&fit=crop&w=200&h=100&q=80',
+    logo: 'https://connectpro.in/images/i-logo.png',
     link: 'https://connectpro.in/',
   },
   {
     id: 2,
     name: 'St. MotherTheresa Uni',
-    logo: 'https://images.unsplash.com/photo-1560472354-b33ff0c44a43?auto=format&fit=crop&w=200&h=100&q=80',
+    logo: 'https://stmothertheresauniversity.com/wp-content/uploads/2022/09/Logo-7.png',
     link: 'https://stmothertheresauniversity.com/',
   },
   {
     id: 3,
     name: 'Ubuntu',
-    logo: 'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?auto=format&fit=crop&w=200&h=100&q=80',
+    logo: 'https://cdn-gmbin.nitrocdn.com/RPiQDeTSlSEXSRLBKIikWQvldqIozJLS/assets/images/optimized/rev-e77e31a/www.ubuntuconsortium.com/wp-content/uploads/2021/08/logo-300x162.jpg',
     link: 'https://www.ubuntuconsortium.com/',
   },
   {
     id: 4,
     name: 'Karnataka Press',
-    logo: 'https://images.unsplash.com/photo-1551434678-e076c223a692?auto=format&fit=crop&w=200&h=100&q=80',
+    logo: 'https://karnatakapressclubcouncil.in/wp-content/uploads/2022/03/Karnataka-Press-01-1-1.png',
     link: 'https://karnatakapressclubcouncil.in/',
   },
   {
     id: 5,
     name: 'Connect Fortunes',
-    logo: 'https://images.unsplash.com/photo-1661956602116-aa6865609028?auto=format&fit=crop&w=200&h=100&q=80',
+    logo: 'https://connectfortunes.com/img/Logo.png',
     link: 'https://connectfortunes.com/',
   },
   {
     id: 6,
     name: 'Bright way',
-    logo: 'https://images.unsplash.com/photo-1661956602116-aa6865609028?auto=format&fit=crop&w=200&h=100&q=80',
+    logo: 'https://brightwayscrap.com/wp-content/uploads/2022/04/BWST-LOGO-1-1.png',
     link: 'https://brightwayscrap.com/',
   },
   {
@@ -44,13 +44,13 @@ export const companyLogos = [
   {
     id: 8,
     name: 'K12 The School Planners',
-    logo: 'https://images.unsplash.com/photo-1661956602116-aa6865609028?auto=format&fit=crop&w=200&h=100&q=80',
+    logo: 'https://k12theschoolplanners.com/images/logo.png',
     link: 'https://k12theschoolplanners.com/',
   },
   {
     id: 9,
     name: 'Ximpasse Groups',
-    logo: 'https://images.unsplash.com/photo-1661956602116-aa6865609028?auto=format&fit=crop&w=200&h=100&q=80',
+    logo: 'https://ximpassegroups.in/wp-content/uploads/2023/08/logo.png',
     link: 'https://ximpassegroups.in/',
   },
 ];
