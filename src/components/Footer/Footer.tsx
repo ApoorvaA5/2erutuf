59be661d791738871ae4a1beaae1dabab16ba730
apoorvaa <apoorvaagowda08@gmail.com>
2025-01-03 15:09:48 +0530

    I'm excited to meet my sis tmrw after 3 years! :)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,31 +10,33 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-black pt-16 pb-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        {/* Flex container to align sections */}
         <div className="flex flex-wrap justify-center gap-16 pb-12 border-b border-gray-700">
+          
           {/* Logo and Description */}
-          <div className="flex flex-col items-center justify-center space-y-4 text-center">
-            <Link to="/" className="flex items-center space-x-2 mb-4">
+          <div className="flex flex-col items-center text-center">
+            <Link to="/" className="flex items-center justify-center space-x-2 mb-4">
               <img src={logo} alt="Future Minds Logo" className="h-20 w-auto" />
               <span className="text-xl font-bold text-white">Future Minds</span>
             </Link>
-            <p className="text-sm text-gray-300">
-              Empowering innovators and investors to build a better future together.
+            <p className="text-sm text-gray-300 mb-4">
+              Empowering innovators and investors <br /> to build a better future together.
             </p>
             <SocialLinks links={socialLinks} />
           </div>
 
           {/* Services Links */}
-          <div className="flex flex-col items-center justify-center text-center">
+          <div className="flex flex-col items-center text-center">
             <FooterLinks title="Services" links={serviceLinks} />
           </div>
 
           {/* Support Links */}
-          <div className="flex flex-col items-center justify-center text-center">
+          <div className="flex flex-col items-center text-center">
             <FooterLinks title="Support" links={supportLinks} />
           </div>
 
           {/* Contact Info */}
-          <div className="flex flex-col items-center justify-center text-center">
+          <div className="flex flex-col items-center text-center">
             <h3 className="font-semibold text-white mb-4">Contact Us</h3>
             <div className="space-y-2 text-sm text-gray-300">
               <p>+91 80266 18661</p>
