05e61b5525a4b1bb710eb2712559b7f6d20c0df6
apoorvaa <apoorvaagowda08@gmail.com>
2025-01-20 16:52:24 +0530

    updated header

--- src/components/Header/Background.tsx
+++ src/components/Header/Background.tsx
@@ -3,35 +3,17 @@ import React from 'react';
 const Background = () => {
   return (
     <>
-      {/* Background Image */}
-      <div 
-        className="absolute inset-0 z-0"
-        style={{
-          backgroundImage: 'url("https://media.gettyimages.com/id/2170886075/photo/close-up-on-hands-filming-chicago.jpg?s=612x612&w=0&k=20&c=CBJYmUo7kMHmrNr7bGP7Vm8U1cKPjIfrZnkGNZlihUQ=")',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
+      {/* Clean Background Video */}
+      <video
+        className="absolute inset-0 z-0 w-full h-full object-cover"
+        poster="/content/dam/public/mastercardcom/in/en/homepage/Home/small_video-still-sunset-over-bridge_2017x1135.jpg"
+        data-src="https://mastercard-a.akamaihd.net/videos/RSA_Header_video_1080p_new.mp4?autoplay=1"
+        src="https://mastercard-a.akamaihd.net/videos/RSA_Header_video_1080p_new.mp4?autoplay=1"
+        autoPlay
+        loop
+        muted
+        playsInline
       />
-      
-      {/* Overlay */}
-      <div className="absolute inset-0 bg-gradient-to-br from-black/70 to-blue-900/50 z-0" />
-      
-      {/* Floating Elements */}
-      <div className="absolute inset-0 z-0">
-        {[...Array(20)].map((_, i) => (
-          <div
-            key={i}
-            className="animate-float absolute"
-            style={{
-              left: `${Math.random() * 100}%`,
-              top: `${Math.random() * 100}%`,
-              animation: `float ${10 + Math.random() * 20}s linear infinite`,
-            }}
-          >
-            <div className="w-2 h-2 bg-white/10 rounded-full backdrop-blur-sm" />
-          </div>
-        ))}
-      </div>
     </>
   );
 };
